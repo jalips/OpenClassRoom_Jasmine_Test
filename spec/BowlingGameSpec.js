cfc00b9e2db41975f36f7f16f28963a1b7be5254
jalips <jalips@protonmail.com>
2019-05-19 19:33:25 +0200

    Chap 2

---
+++ spec/BowlingGameSpec.js
@@ -0,0 +1,27 @@
+describe("Bowling game", function() {
+	var game;
+
+	beforeEach(function(){
+		game = new BowlingGame();
+	});
+
+	function rollMany (n, pins) {
+        for (var i = 0; i < n; i++) {
+            game.roll(pins)
+        }
+    }
+        
+    it("should properly calculate a gutter game", function() {
+	    rollMany(20, 0);
+	    expect(game.score()).toEqual(0);
+	});
+    
+    it("should properly calculate a strike", function () {
+        // test
+    });
+    
+    it("should properly calculate a spare", function () {
+        // test
+    });
+        
+});
